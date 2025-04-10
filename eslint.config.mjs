8a9fa61a04eafca6a2120b28b8b515418af8f3e4
hopkins385 <stadhouders.sven@gmail.com>
2025-04-10 18:34:45 +0200

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,7 @@ export default tseslint.config(
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   }
 );
